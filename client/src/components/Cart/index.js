5a383712580dc16096a777ad1ebc47577eb9a84a
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-12-29 02:11:29 -0800

    make shopping cart funcinal

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -13,6 +13,14 @@ const Cart = () => {
         dispatch({ type: TOGGLE_CART});
     };
 
+    function calculateTotal() {
+        let sum = 0;
+        state.cart.forEach(item => {
+            sum += item.price * item.purchaseQuantity;
+        });
+        return sum.toFixed(2);
+    }
+
     if (!state.cartOpen) {
         return (
             <div className="cart-closed" onClick={toggleCart}>
@@ -26,23 +34,33 @@ const Cart = () => {
         <div className="cart">
             <div className="close" onClick={toggleCart}>[close]</div>
             <h2>Shopping Cart</h2>
-            <div>
-                <CartItem item={{name:'Camera', image:'camera.jpg', price:5, purchaseQuantity:3}} />
-                <CartItem item={{name:'Soap', image:'soap.jpg', price:6, purchaseQuantity:4}} />
-
+            {state.cart.length ? (
+                <div>
+                {state.cart.map(item => (
+                    <CartItem key={item._id} item={item} />
+                ))}
                 <div className="flex-row space-between">
-                    <strong>Total: $0</strong>
+                    <strong>Total: ${calculateTotal()}</strong>
                     {
-                        Auth.loggedIn() ?
+                    Auth.loggedIn() ?
                         <button>
-                            Checkout
+                        Checkout
                         </button>
                         :
                         <span>(log in to check out)</span>
                     }
                 </div>
-            </div>
+                </div>
+            ) : (
+                <h3>
+                <span role="img" aria-label="shocked">
+                    😱
+                </span>
+                You haven't added anything to your cart yet!
+                </h3>
+            )}
         </div>
+
     );
 };
 
