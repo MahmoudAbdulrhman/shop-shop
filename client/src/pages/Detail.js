e2275d0cd387ab675a5c9249421815d06ef3f25e
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-12-28 23:59:35 -0800

    update state

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
+import { useStoreContext } from "../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../utils/actions";
 
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 
 function Detail() {
-  const { id } = useParams();
+  const [state, dispatch] = useStoreContext();
+const { id } = useParams();
 
-  const [currentProduct, setCurrentProduct] = useState({})
+const [currentProduct, setCurrentProduct] = useState({})
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+const { products } = state;
 
-  useEffect(() => {
-    if (products.length) {
-      setCurrentProduct(products.find(product => product._id === id));
-    }
-  }, [products, id]);
+useEffect(() => {
+  if (products.length) {
+    setCurrentProduct(products.find(product => product._id === id));
+  } else if (data) {
+    dispatch({
+      type: UPDATE_PRODUCTS,
+      products: data.products
+    });
+  }
+}, [products, data, dispatch, id]);
 
   return (
     <>
