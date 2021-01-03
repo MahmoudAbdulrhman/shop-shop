b92b69bafe8dc46a79c207ec1d1ce694b44a5e67
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2021-01-02 23:26:12 -0800

    add offline

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -32,11 +32,17 @@ const addToCart = () => {
       _id: id,
       purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
     });
+    // if  we're updating quantity, use existing item data and icrement purchaseQuantity value by one 
+    idbPromise('cart', 'put', {
+      purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+    });
   } else {
     dispatch({
       type: ADD_TO_CART,
       product: { ...currentProduct, purchaseQuantity: 1 }
     });
+    // if product isn`t in the cart yet, add it to the current shopping cart in IndexedDB
+    idbPromise('cart', 'put', {...currentProduct, purchaseQuantity: 1});
   }
 };
 
@@ -45,6 +51,8 @@ const removeFromCart = () => {
     type: REMOVE_FROM_CART,
     _id: currentProduct._id
   });
+  // upon removal from cart, delete the item from IndexDB using the  `currentProduct._id` to locate what to remove
+  idbPromise('cart', 'delete', {...currentProduct});
 }
 
 useEffect(() => {
